c756e0b8ed1c269a719ae8590f2529a1bf8c2445
Sanshyashree123 <Msanthiyamsandhya@gmail.com>
2023-02-01 11:49:56 +0530

    Sample Practice push

---
+++ src/com/maven/Sample.java
@@ -0,0 +1,11 @@
+package com.maven;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "shrini";
+		System.out.println(s);
+	}
+
+}
